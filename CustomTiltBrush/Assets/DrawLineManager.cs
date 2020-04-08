39c0543af4d267b4f79e7a923e04c3f1770b7595
abhishek-sharma1042 <62215275+abhishek-sharma1042@users.noreply.github.com>
2020-04-08 12:21:19 -0700

    initial commit

---
+++ CustomTiltBrush/Assets/DrawLineManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class DrawLineManager : MonoBehaviour
+{
+
+    public Material material;
+    private SteamVR_Action_Boolean start = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
+    private SteamVR_Action_Boolean keepAddingPoints = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("TriggerHold");
+    private MeshLineRenderer currLine; 
+    public SteamVR_TrackedObject trackedObj;
+    private SteamVR_Input_Sources source = SteamVR_Input_Sources.RightHand;
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("trigger just clicked: "+start.GetStateDown(source));
+        Debug.Log("trigger being held: "+keepAddingPoints.GetState(source));
+        if(start.GetStateDown(source)){
+            GameObject go = new GameObject();
+            go.AddComponent<MeshFilter>();
+            go.AddComponent<MeshRenderer>();
+
+            currLine = go.AddComponent<MeshLineRenderer>();
+            currLine.setWidth (0.1f);
+            currLine.lmat = material;
+           
+           
+        }
+        if(keepAddingPoints.GetState(source)){
+           
+            Debug.Log("still here");
+            
+            currLine.AddPoint(trackedObj.transform.position);
+            
+        }
+    }
+}
---
+++ CustomTiltBrush/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+	public SteamVR_Action_Vector2 input;
+	public float speed = 1;
+	private LineRenderer currLine;
+	private CharacterController characterController;
+
+
+	void Start(){
+		characterController = GetComponent<CharacterController>();
+	}
+    // Update is called once per frame
+    void Update()
+    {
+		// Debug.Log(input.axis.x + "   "+ input.axis.y);
+		Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+		// transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0,9.81f,0) * Time.deltaTime;
+		characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction,Vector3.up) - new Vector3(0,9.81f,0) * Time.deltaTime);
+    }
+}
